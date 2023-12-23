36710b7f5b9177a996888999945b290a8444fba6
Aashutosh Agrawal <96795262+Aashu1303@users.noreply.github.com>
2023-12-23 17:17:50 +0530

    Time: 5 ms (19.89%) | Memory: 7.2 MB (64.64%) - LeetSync

--- 1619-path-crossing/path-crossing.cpp
+++ 1619-path-crossing/path-crossing.cpp
@@ -1,7 +1,17 @@
+struct pair_hash {
+    template <class T1, class T2>
+    std::size_t operator () (const std::pair<T1, T2>& p) const {
+        auto h1 = std::hash<T1>{}(p.first);
+        auto h2 = std::hash<T2>{}(p.second);
+
+        // Simple hash combining technique
+        return h1 ^ h2;
+    }
+};
 class Solution {
 public:
     bool isPathCrossing(string path) {
-        map<pair<int,int>,int> mp;
+        unordered_map<pair<int,int>,int , pair_hash> mp;
         int x = 0 , y = 0;
         mp[{0,0}]++;
         for(auto i : path){
