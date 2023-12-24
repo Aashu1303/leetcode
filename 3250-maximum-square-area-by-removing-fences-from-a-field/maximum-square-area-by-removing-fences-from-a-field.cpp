f3bd64e39d0bb252ea6a0026acc55641b8204730
Aashutosh Agrawal <96795262+Aashu1303@users.noreply.github.com>
2023-12-24 17:21:35 +0530

    Time: 1000 ms (100.00%) | Memory: 262.6 MB (100.00%) - LeetSync

--- 3250-maximum-square-area-by-removing-fences-from-a-field/maximum-square-area-by-removing-fences-from-a-field.cpp
+++ 3250-maximum-square-area-by-removing-fences-from-a-field/maximum-square-area-by-removing-fences-from-a-field.cpp
@@ -1,29 +1,31 @@
 class Solution {
+     unordered_set<int> make(vector<int> &v, int p) {
+         v.push_back(1);
+         v.push_back(p);
+         unordered_set<int> s;
+         for (int i = 0; i < v.size(); ++i) {
+             for (int j = 0; j < i; ++j) {
+                 s.insert(abs(v[i] - v[j]));  
+             }
+         }
+        return s;
+    }
 public:
     int maximizeSquareArea(int m, int n, vector<int>& hFences, vector<int>& vFences) {
-        if(m == n){
-            return (m-1)*(n-1);
-        }
-        long long MOD = 1e9+7 , ans = -1;
-        hFences.insert(hFences.begin() ,{1,m});
-        vFences.insert(vFences.begin() , {1,n});
-        sort(vFences.begin() , vFences.end());
-        sort(hFences.begin() , hFences.end());
-        map<int ,int> mp;
-        
-        for(int i = 0 ; i < hFences.size() ; i++){
-            for(int j = i+1 ; j < hFences.size() ; j++){
-                mp[hFences[j] - hFences[i]]++;
-            }
-        }
-        for(int i = 0 ; i < vFences.size() ; i++){
-            for(int j = i+1 ; j < vFences.size() ; j++){
-                long long tem = vFences[j] - vFences[i];
-                if(mp.count(tem)){
-                    ans = max(ans , tem*tem);
+        const auto& s = make(hFences, m);
+        vFences.push_back(1);
+        vFences.push_back(n);
+        int e = 0;
+        for (int i = 0; i < vFences.size(); ++i) {
+            for (int j = 0; j < i; ++j) {
+                const int x = abs(vFences[i] - vFences[j]);
+                if (x > e && s.count(x)) {
+                    e = x;
                 }
             }
+            
         }
-        return ans % MOD;
+        return e ? (1LL * e * e) % 1000000007 : -1;
+        
     }
 };
