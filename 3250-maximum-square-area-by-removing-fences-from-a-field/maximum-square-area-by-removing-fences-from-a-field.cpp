c99ab0134f7294b7824b48490c1c8e6feeef1ac6
Aashutosh Agrawal <96795262+Aashu1303@users.noreply.github.com>
2023-12-24 17:13:50 +0530

    Time: 2843 ms (10.00%) | Memory: 276 MB (60.00%) - LeetSync

--- 3250-maximum-square-area-by-removing-fences-from-a-field/maximum-square-area-by-removing-fences-from-a-field.cpp
+++ 3250-maximum-square-area-by-removing-fences-from-a-field/maximum-square-area-by-removing-fences-from-a-field.cpp
@@ -9,7 +9,7 @@ public:
         vFences.insert(vFences.begin() , {1,n});
         sort(vFences.begin() , vFences.end());
         sort(hFences.begin() , hFences.end());
-        unordered_map<int ,int> mp;
+        map<int ,int> mp;
         
         for(int i = 0 ; i < hFences.size() ; i++){
             for(int j = i+1 ; j < hFences.size() ; j++){
@@ -19,7 +19,7 @@ public:
         for(int i = 0 ; i < vFences.size() ; i++){
             for(int j = i+1 ; j < vFences.size() ; j++){
                 long long tem = vFences[j] - vFences[i];
-                if(mp[tem]){
+                if(mp.count(tem)){
                     ans = max(ans , tem*tem);
                 }
             }
