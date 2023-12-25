bcfe2ef265df7e7b42da1263d066d844bf2b8a0d
Aashutosh Agrawal <96795262+Aashu1303@users.noreply.github.com>
2023-12-26 01:54:45 +0530

    Time: 0 ms (100.00%) | Memory: 6.5 MB (65.60%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -2,20 +2,30 @@ class Solution {
 public:
     int numDecodings(string s) {
         int n = s.length();
-        if(n == 0 || s[0] == '0') return 0;
-        vector<int> dp(2,1);
-
-        for(int i = 1 ; i < n ; i++){
-            int ways = 0;
-            if(s[i] != '0'){
-                ways += dp[1]; 
-            }
-            if(s[i-1] =='1' || (s[i-1] == '2' && s[i] <= '6')){
-                ways += dp[0];
-            }
-            dp[0] = dp[1];
-            dp[1] = ways;
+        if(n == 1 || s[0] == '0') return (s[0] == '0') ? 0:1;
+        vector<int> dp(n , 0);
+        if(s[0] != '0'){
+            dp[0] = 1;
         }
-        return dp[1];
+        int y = s[1] - '0' + (s[0] - '0') * 10;
+        if(y >= 10 && y <= 26){
+            dp[1]++;
+        }
+        if(s[1] > '0'){
+            dp[1]++;
+        }
+
+        for(int i = 2 ; i < n ; i++){
+            int op1 = s[i] - '0';
+            int op2 = s[i] - '0' + (s[i-1] - '0') * 10;
+
+            if(op1 != 0){
+                dp[i] += dp[i-1];
+            }
+            if(op2 >= 10 && op2 <= 26)
+                dp[i] += dp[i-2];
+        }
+
+        return dp[n-1];
     }
 };
