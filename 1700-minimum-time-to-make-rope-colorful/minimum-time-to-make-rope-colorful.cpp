be7a3d5da35d3ec454cc79a334e8bd4275246bfa
Aashutosh Agrawal <96795262+Aashu1303@users.noreply.github.com>
2023-12-27 17:18:50 +0530

    Time: 352 ms (5.22%) | Memory: 95.6 MB (83.31%) - LeetSync

---
+++ 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minCost(string s, vector<int>& v) {
+        int n = s.size();
+        if(s.size() == 1) return 0;
+        char prev = s[0]; int prevMin = INT_MAX , ans = 0 , prevIndex =0;
+        for(int i = 1 ; i < n ; i++){
+            char curr = s[i];
+            if(curr == prev){
+                prevMin = min(v[prevIndex] , v[i]);
+                ans += prevMin;
+                if(v[prevIndex] < v[i]) prevIndex = i;
+            }else{
+                prevIndex = i;
+            }
+            prev = curr;
+            cout << prevIndex << " ";
+        }
+        cout << endl;
+        return ans;
+    }
+};
