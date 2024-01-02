b5db981fac9a6260ce960f82b0331a18b7705fc1
Aashutosh Agrawal <96795262+Aashu1303@users.noreply.github.com>
2024-01-02 18:25:24 +0530

    Time: 41 ms (5.35%) | Memory: 29.9 MB (83.60%) - LeetSync

--- 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.cpp
+++ 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.cpp
@@ -9,7 +9,7 @@ public:
         int maxm = *max_element(freq.begin() , freq.end());
         cout << maxm << endl;
         vector<vector<int>> ans(maxm);
-        unordered_map<int,int> mp;
+        map<int,int> mp;
         for(int i = 0 ; i < n ; i++){
             ans[mp[nums[i]]].push_back(nums[i]);
             mp[nums[i]]++;
