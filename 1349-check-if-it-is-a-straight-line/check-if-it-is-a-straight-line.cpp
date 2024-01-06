e4862512e0df016cdbf21eea21558c8ac8ce9320
Aashutosh Agrawal <96795262+Aashu1303@users.noreply.github.com>
2024-01-06 23:56:25 +0530

    Time: 11 ms (34.00%) | Memory: 10.7 MB (8.03%) - LeetSync

---
+++ 1349-check-if-it-is-a-straight-line/check-if-it-is-a-straight-line.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool checkStraightLine(vector<vector<int>>& v) {
+        int n = v.size();
+        if(n == 2) return true;
+        double x = v[0][0] , y = v[0][1];
+        double prev; 
+        if(x == v[1][0]){
+            prev = DBL_MAX;
+        }else{
+            prev = (v[1][1] - y) / (v[1][0] - x);
+        }
+        for(int i = 2 ; i < n;  i++){
+            double curr = DBL_MAX;
+            if(v[i][0] != x){
+                curr = (v[i][1] - y) / (v[i][0] - x);
+            }
+            if(curr != prev){
+                return false;
+            } 
+        }
+        return true;
+    }
+};
