7684e3b283a0845b2aa6e84ca73122cb33ccf3a9
Aashutosh Agrawal <96795262+Aashu1303@users.noreply.github.com>
2024-01-16 18:04:32 +0530

    Time: 161 ms (87.66%) | Memory: 97.4 MB (73.46%) - LeetSync

--- 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.cpp
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.cpp
@@ -1,38 +1,40 @@
 class RandomizedSet {
-    std::set<int> s;
-    std::vector<int> elements;  // Additional vector to store elements
+    unordered_map<int,int> mp;
+    vector<int> v;
 public:
     RandomizedSet() {
     }
     
     bool insert(int val) {
-        if(s.find(val) != s.end()) return false;
-        s.insert(val);
-        elements.push_back(val);  // Add to the vector as well
+        if(mp.find(val) != mp.end()) return false;
+        v.push_back(val);
+        mp[val] = v.size()-1;
         return true;
     }
     
     bool remove(int val) {
-        auto it = s.find(val);
-        if(it == s.end()) return false;
-
-        s.erase(it);
-        elements.erase(std::remove(elements.begin(), elements.end(), val), elements.end());  // Remove from the vector
-        return true; 
+       if(mp.find(val) == mp.end()) return false;
+       int i = mp[val];
+       mp[v.back()] = i; 
+       // transfering this index element to last 
+       swap(v[i] , v[v.size()-1]);
+       v.pop_back(); // removing the val from the array 
+       mp.erase(val); // removing val from map
+       return true; 
     }
     
     int getRandom() {
-        if (elements.empty()) {
-            // Handle the case when the set is empty
-            // You might want to throw an exception or return a default value
-        }
-
-        // Use a random index to get an element from the vector
-        std::random_device rd;
-        std::mt19937 gen(rd());
-        std::uniform_int_distribution<> dis(0, elements.size() - 1);
-
-        int randomIndex = dis(gen);
-        return elements[randomIndex];
+        /** if a number is divided by k then the 
+        domain of remainders will be defined from [0,k-1];
+        **/
+        return v[rand()%(v.size())]; 
     }
 };
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet* obj = new RandomizedSet();
+ * bool param_1 = obj->insert(val);
+ * bool param_2 = obj->remove(val);
+ * int param_3 = obj->getRandom();
+ */
