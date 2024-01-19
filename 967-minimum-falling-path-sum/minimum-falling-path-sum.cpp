b4f72432bf2ad5a0cff9477ab93d881bf819a722
Aashutosh Agrawal <96795262+Aashu1303@users.noreply.github.com>
2024-01-20 00:19:02 +0530

    Time: 16 ms (31.12%) | Memory: 13 MB (5.13%) - LeetSync

---
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -0,0 +1,40 @@
+class Solution {
+    int n;
+
+    int solve(vector<vector<int>>& v, int row, int col, vector<vector<int>>& dp) {
+        if (dp[row][col] != INT_MAX)
+            return dp[row][col];
+
+        if (row == n - 1)
+            return dp[row][col] = v[row][col];
+
+        int left = INT_MAX, right = INT_MAX;
+
+        if (col > 0)
+            left = solve(v, row + 1, col - 1, dp);
+
+        int down = solve(v, row + 1, col, dp);
+
+        if (col < n - 1)
+            right = solve(v, row + 1, col + 1, dp);
+
+        dp[row][col] = min(left, min(down, right)) + v[row][col];
+
+        return dp[row][col];
+    }
+
+public:
+    int minFallingPathSum(vector<vector<int>>& v) {
+        n = v.size();
+        if (n == 1)
+            return v[0][0];
+        vector<vector<int>> dp(n, vector<int>(n, INT_MAX));
+        int ans = INT_MAX;
+
+        for (int col = 0; col < n; col++) {
+            ans = min(ans, solve(v, 0, col, dp));
+        }
+
+        return ans;
+    }
+};
