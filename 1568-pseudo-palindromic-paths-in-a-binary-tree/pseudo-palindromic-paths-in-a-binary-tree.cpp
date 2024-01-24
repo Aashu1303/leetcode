c5e0e70dc779877f2e5767955f9c948125432d0c
Aashutosh Agrawal <96795262+Aashu1303@users.noreply.github.com>
2024-01-24 18:25:08 +0530

    Time: 241 ms (93.03%) | Memory: 177.7 MB (98.66%) - LeetSync

--- 1568-pseudo-palindromic-paths-in-a-binary-tree/pseudo-palindromic-paths-in-a-binary-tree.cpp
+++ 1568-pseudo-palindromic-paths-in-a-binary-tree/pseudo-palindromic-paths-in-a-binary-tree.cpp
@@ -1,47 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
+    int ans = 0;
+    void traversal(TreeNode * root , vector<int> &v){
+        if(root == NULL){
+            return;
+        }
+        v[root -> val]++;
+        if(root -> left == NULL && root -> right == NULL){
+            int count =0;
+            for(auto i : v){
+                if(i % 2 != 0) count++;
+            }
+            ans += (count <= 1) ? 1 : 0;
+        }
+
+        traversal(root -> left , v);
+        traversal(root -> right , v);
+        v[root -> val]--;
+    }
+
 public:
-    // Function to count pseudo-palindromic paths in a binary tree.
-    int pseudoPalindromicPaths(TreeNode* root) {
-
-        vector<int> digits(10, 0);
-        // Start DFS traversal from the root.
-        return dfs(root, digits);
-    }
-
-private:
-    // Recursive function for DFS traversal.
-    int dfs(TreeNode* root, vector<int>& digits) {
-        // Base case: If the current node is NULL, return 0.
-        if (root == nullptr) return 0;
-
-        // Increment the count of the current digit.
-        digits[root->val]++;
-
-        // If the current node is a leaf, check if the path is pseudo-palindromic.
-        if (root->left == nullptr && root->right == nullptr) {
-            int cnt = countOdd(digits);
-            digits[root->val]--;  // Backtrack by decrementing the count.
-           
-            return (cnt <= 1) ? 1 : 0;
-        }
-
-        // Recursively traverse the left and right subtrees.
-        int leftCount = dfs(root->left, digits);
-        int rightCount = dfs(root->right, digits);
-
-        // Backtrack by decrementing the count of the current digit.
-        digits[root->val]--;
-
-        return leftCount + rightCount;
-    }
-
-    // Helper function to count the number of digits with odd occurrences.
-    int countOdd(const vector<int>& digits) {
-        int cnt = 0;
-        
-        for (int i = 1; i < 10; ++i) {
-            if ((digits[i]) & 1) cnt++;
-        }
-        return cnt;
+    int pseudoPalindromicPaths (TreeNode* root) {
+        vector<int> temp(10,0);
+        traversal(root,temp);
+        return ans;        
     }
 };
