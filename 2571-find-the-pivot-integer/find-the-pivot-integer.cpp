5c330966fe7277ee1a2e416b8fdbff0a91adf3f4
Aashutosh Agrawal <96795262+Aashu1303@users.noreply.github.com>
2024-03-13 11:02:29 +0530

    Time: 3 ms (51.39%) | Memory: 7.1 MB (73.82%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
@@ -4,10 +4,11 @@ public:
         if(n == 1) return 1;
         int sum = n * (n+1) / 2;
         int prev = 0 , k = n/2;
-        while(n >= k){
+        while(n != k){
             prev += n;
             //cout << sum << " " << prev << endl;
             if(sum == prev) return n;
+            if(sum < prev) return -1;
             sum -= n;
             n--;
         }
