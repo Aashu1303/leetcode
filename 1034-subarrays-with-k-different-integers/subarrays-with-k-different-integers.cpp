9c3d2cf6e10793ab89aa3f0b69055812e80d0f81
Aashutosh Agrawal <96795262+Aashu1303@users.noreply.github.com>
2024-03-30 18:43:49 +0530

    Time: 80 ms (85.44%) | Memory: 43.1 MB (89.89%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.cpp
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.cpp
@@ -1,26 +1,35 @@
 class Solution {
-    int solve(int k , vector<int>& nums){
-        int n = nums.size();
-        vector<int> v(n+1,0);
-        int count = 0 , ans = 0;
-        int i = 0 , j = 0;
-        while(j < n){
-            v[nums[j]]++;
-            if(v[nums[j]] == 1) count++;
-            while(count > k && i < j){
-                v[nums[i]]--;
-                if(v[nums[i]] == 0) count--;
-                i++;
-            }
-            ans += j-i+1;
-            j++;
-        }
-        return ans;
-    }
 public:
     int subarraysWithKDistinct(vector<int>& nums, int k) {
-        if(k == 1) return solve(1,nums);
-        int ans = solve(k,nums) - solve(k-1,nums);
+        int n= nums.size();
+        int i = 0 , j = 0 , l = 0 ;// (i_bada);
+        int ans = 0;
+        // vector<int> v(n+1);
+        // using hashmap 
+        unordered_map<int,int> mp;
+        while(j < n){
+            mp[nums[j]]++;
+
+            while(mp.size() > k){
+                mp[nums[i]]--;
+                if(mp[nums[i]] == 0){
+                    mp.erase(nums[i]);
+                }
+                i++;
+                l = i;
+            }
+
+            while(mp[nums[i]] > 1){
+                mp[nums[i]]--;
+                i++;
+            }
+
+            if(mp.size() == k){
+                ans += (1 + i - l);
+            }
+            j++;
+        }
+
         return ans;
     }
 };
