e7f2ea1c1f35110bf5734af5c82d3781b812f4b5
Aashutosh Agrawal <96795262+Aashu1303@users.noreply.github.com>
2024-04-13 03:04:47 +0530

    Time: 8 ms (72.79%) | Memory: 21.8 MB (14.83%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -11,7 +11,7 @@
  */
 class Solution {
     int solve(TreeNode* root){
-        if(root == NULL) return 1;
+        if(root == NULL) return 0;
         int left = solve(root -> left);
         int right = solve(root -> right);
         if(left == -1 || right == -1) return -1;
