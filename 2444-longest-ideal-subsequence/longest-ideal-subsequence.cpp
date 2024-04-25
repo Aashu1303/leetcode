f67a8a93b0265af1b1d5cffa5a53d449a34ef361
Aashutosh Agrawal <96795262+Aashu1303@users.noreply.github.com>
2024-04-26 01:42:08 +0530

    Time: 55 ms (61.51%) | Memory: 15.2 MB (49.37%) - LeetSync

--- 2444-longest-ideal-subsequence/longest-ideal-subsequence.cpp
+++ 2444-longest-ideal-subsequence/longest-ideal-subsequence.cpp
@@ -3,17 +3,17 @@ public:
     int longestIdealString(string s, int k) {
         int ans = 1;
         int n = s.size();
-        vector<int> dp(n+1, 1);
-        unordered_map<char,int> mp;
-        mp[s[0]]++;
+        vector<int> dp(n, 1);
+        vector<int> mp(26,0);
+        mp[s[0]-'a']++;
         for(int i = 1 ; i < n ; i++){
             int mn = (s[i] - k) , mx = (s[i] + k);
             for(char c = 'a' ; c <= 'z' ; c++){
                 if(c >= mn && c <= mx){
-                    dp[i] = max(dp[i] , mp[c]+1);
+                    dp[i] = max(dp[i] , mp[c-'a']+1);
                 }
             }
-            mp[s[i]] = max(dp[i] , mp[s[i]]);
+            mp[s[i]-'a'] = max(dp[i] , mp[s[i]-'a']);
             ans = max(ans , dp[i]);
         }
 
