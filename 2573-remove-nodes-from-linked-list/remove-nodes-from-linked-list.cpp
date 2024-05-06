66a2416ebb14ced1d9c898138797ebd355386f2d
Aashutosh Agrawal <96795262+Aashu1303@users.noreply.github.com>
2024-05-06 12:27:08 +0530

    Time: 222 ms (99.15%) | Memory: 164.4 MB (65.64%) - LeetSync

--- 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.cpp
+++ 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.cpp
@@ -12,26 +12,12 @@ class Solution {
 public:
     ListNode* removeNodes(ListNode* head) {
         ios_base::sync_with_stdio(0);
-        ListNode* curr = head;
-        stack<ListNode*> st;
-        while(curr != NULL){
-            int temp = curr -> val;
-            while(!st.empty() && (st.top()->val) < temp){
-                st.pop();
-            }
-            st.push(curr);
-            curr = curr -> next;
+        if(head == NULL) return head;
+        head -> next = removeNodes(head -> next);
+        if(head -> next == NULL) return head;
+        if(head->next->val > head->val){
+            return head -> next;
         }
-
-        ListNode* ans = NULL;
-        while(!st.empty()){
-            ListNode * temp = st.top();
-            cout << temp -> val << " ";
-            st.pop();
-            temp -> next = ans;
-            ans = temp;
-        }
-
-        return ans;
+        return head;
     }
 };
