ac5d7053aaf50d9e82260cb76e0a1573f3b2eb6c
Aashutosh Agrawal <96795262+Aashu1303@users.noreply.github.com>
2024-05-14 23:24:06 +0530

    Time: 151 ms (73.69%) | Memory: 107.1 MB (61.49%) - LeetSync

--- 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.cpp
+++ 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.cpp
@@ -4,12 +4,11 @@ public:
     long long maximumHappinessSum(vector<int>& h, int k) {
        long long ans = 0;
        sort(h.begin() , h.end() , greater<int>());
-       int count = 0 , j = 0;
+       int count = 0;
        for(int i = 0 ; i < k ; i++){
-            if(count > h[j]) break;
-            ans += abs(h[j] - count);
+            if(count > h[i]) break;
+            ans += abs(h[i] - count);
             count++;
-            j++;
        }
        return ans;
     }
