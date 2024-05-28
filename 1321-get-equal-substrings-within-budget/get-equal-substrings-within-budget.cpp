1f81f661f8a6b61fab5678bdc2392911a07dadfa
Aashutosh Agrawal <96795262+Aashu1303@users.noreply.github.com>
2024-05-28 18:09:32 +0530

    Time: 34 ms (5.56%) | Memory: 9.5 MB (12.01%) - LeetSync

---
+++ 1321-get-equal-substrings-within-budget/get-equal-substrings-within-budget.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int equalSubstring(string s, string t, int maxCost) {
+        int n = s.size();
+        vector<int> prefix(n+1 , 0);
+        for(int i = 0 ; i < n ; i++){
+            prefix[i+1] = prefix[i] + abs(s[i]-t[i]); 
+        }
+        int ans = 0;
+        for(auto i : prefix) cout << i << " ";
+        cout << endl;
+        for(int i = 1 ; i <= n ; i++){
+            int x = lower_bound(prefix.begin() , prefix.begin() + i , prefix[i]-maxCost) - prefix.begin();
+            ans = max(ans , i-x);
+        }
+
+        return ans;
+    }
+};
