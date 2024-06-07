e9cc5771054d3ea97905e4814eec96dcc7ae6f33
Aashutosh Agrawal <96795262+Aashu1303@users.noreply.github.com>
2024-06-07 19:13:40 +0530

    Time: 1404 ms (5.11%) | Memory: 383.7 MB (16.16%) - LeetSync

---
+++ 648-replace-words/replace-words.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    string replaceWords(vector<string>& dictionary, string sentence) {
+        string ans;
+        map<string,int> mp;
+        for(auto i : dictionary){
+            mp[i]++;
+        }
+        string temp;
+        for(int i = 0 ; i < sentence.size() ; i++){
+            if(sentence[i] == ' '){
+                string curr , to_push;
+                for(int j = 0 ; j < temp.size() ; j++){
+                    curr += temp[j];
+                    if(mp[curr] > 0){
+                        to_push = curr;
+                        break;
+                    } 
+                }
+                if(to_push.empty()){
+                    ans += curr;
+                }else{
+                    ans += to_push;
+                }
+                ans.push_back(' ');
+                temp = "";
+            }
+            else temp += sentence[i];
+        }
+
+
+        string curr , to_push;
+        for(int j = 0 ; j < temp.size() ; j++){
+            curr += temp[j];
+            if(mp[curr] > 0) to_push = curr;
+        }
+        if(to_push.empty()){
+            ans += curr;
+        }else{
+            ans += to_push;
+        }
+        
+        return ans;
+    }
+};
