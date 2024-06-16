e856aeea952255248003d8034cb3145a5e75bce6
Aashutosh Agrawal <96795262+Aashu1303@users.noreply.github.com>
2024-06-16 10:03:14 +0530

    Time: 703 ms (60.00%) | Memory: 226.3 MB (60.00%) - LeetSync

--- 3437-maximum-total-damage-with-spell-casting/maximum-total-damage-with-spell-casting.cpp
+++ 3437-maximum-total-damage-with-spell-casting/maximum-total-damage-with-spell-casting.cpp
@@ -1,36 +1,37 @@
 class Solution {
-long long dp[100001];
-    map<long long,long long> m;
-    long long solve(int i,vector<long long>& v)
+    map<long long ,long long> m;
+    long long solve(int i,vector<long long>& v,vector<long long>& dp)
     {
         if(i>=v.size())
         {
             return 0;
         }
-        if(dp[i] != -1)
+        if(dp[i]!=-1)
         {
             return dp[i];
         }
         long long not_take=0,take=0;
         if(i+1<v.size() && v[i+1]>v[i]+2)
         {
-            take=(1ll*v[i]*m[v[i]])+solve(i+1,v);
+            take=(1ll*v[i]*m[v[i]])+solve(i+1,v,dp);
                 
         }
         else if(i+2<v.size() && v[i+2]>v[i]+2)
         {
-            take=(1ll*v[i]*m[v[i]])+solve(i+2,v);   
+            take=(1ll*v[i]*m[v[i]])+solve(i+2,v,dp);   
         }
         else
         {
-            take=(1ll*v[i]*m[v[i]])+solve(i+3,v);  
+            take=(1ll*v[i]*m[v[i]])+solve(i+3,v,dp);  
         }
-        not_take=solve(i+1,v);
+        not_take=solve(i+1,v,dp);
         
         return dp[i]=max(take,not_take);
     }
 public:
     long long maximumTotalDamage(vector<int>& power) {
+       
+        vector<long long> dp(power.size()+1,-1);
         vector<long long> v;
         for(int i=0;i<power.size();i++)
         {
@@ -41,7 +42,6 @@ public:
             m[power[i]]++;
         }
         sort(v.begin(),v.end());
-        memset(dp,-1,sizeof(dp));
-        return solve(0,v);
+        return solve(0,v,dp);
     }
 };
